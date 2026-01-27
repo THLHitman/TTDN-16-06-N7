6c917ab710de51bb47c88b01c802e6fa84bcefa2
THLHitman <lichta61@gmail.com>
2026-01-27 13:43:08 +0700

    modified:   addons/quan_ly_phong_hop/models/__init__.py 	modified:   addons/quan_ly_phong_hop/models/booking.py 	deleted:    addons/quan_ly_phong_hop/models/room_booking_asset_check.py

--- addons/quan_ly_phong_hop/models/room_booking_asset_check.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-
-class RoomBookingAssetCheck(models.Model):
-    _name = 'room.booking.asset.check'
-    _description = 'Kiểm tra tài sản phòng họp'
-    _order = 'check_time desc'
-
-    
